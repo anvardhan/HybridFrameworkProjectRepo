fd58c744e7693805cbab2fa94de426ccff87011d
Anand Vardhan <anand.vn100@gmail.com>
2022-03-17 23:30:38 -0500

    feature to master

--- src/main/java/com/qa/pages/FinologyLoginPage.java
+++ src/main/java/com/qa/pages/FinologyLoginPage.java
@@ -52,6 +52,7 @@ public class FinologyLoginPage extends BrowserDriverFactory {
 	//Methods/Actions related to LoginPage
 	public void TestFeatureBranch () {
 		System.out.println("Added code into master branch by other developer");
+		System.out.println("Added code into from feature branch by me");
 	}
 		
 
